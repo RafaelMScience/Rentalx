66e3a7f9b56cb6092bf065578c7615d387da3ff0
RafaelM <rafaelm.science@gmail.com>
2021-10-12 18:40:21 -0400

    feat: Utilizando o Princípio da Substituição de Liskov (LSP)

--- src/services/CreateCategoryService.ts
+++ src/services/CreateCategoryService.ts
@@ -1,14 +1,13 @@
-import { CategoriesRepository } from '../repositories/CategoriesRepository';
+import { ICategoriesRepository } from '../repositories/ICategoriesRepository';
 
 interface IRequest {
     name: string;
-
     description: string;
 }
 
 class CreateCategoryService {
     // eslint-disable-next-line prettier/prettier
-    constructor(private categoriesRepository: CategoriesRepository) { }
+    constructor(private categoriesRepository: ICategoriesRepository) { }
 
     execute({ description, name }: IRequest): void {
         const categoryAlreadyExists =
