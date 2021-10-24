ff6175e0f68de2ba8fdac4c6048c50f8f477e6a6
RafaelM <rafaelm.science@gmail.com>
2021-10-24 10:44:42 -0400

    feat: Criando repositório de especificação

--- src/modules/cars/services/CreateSpecificationService.ts
+++ src/modules/cars/services/CreateSpecificationService.ts
@@ -1,6 +1,24 @@
+import { ISpecificationsRepository } from '../repositories/ISpecificationsRepository';
+
+interface IRequest {
+    name: string;
+    description: string;
+}
+
 class CreateSpecificationService {
-    execute(): void {
-        console.log('TODO');
+    // eslint-disable-next-line prettier/prettier
+    constructor(private specificationsRepository: ISpecificationsRepository) { }
+    execute({ name, description }: IRequest): void {
+        const specificationsAlreadyExists =
+            this.specificationsRepository.findByName(name);
+
+        if (specificationsAlreadyExists) {
+            throw new Error('Specification already exists!');
+        }
+        this.specificationsRepository.create({
+            name,
+            description,
+        });
     }
 }
 
